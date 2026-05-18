1df96365232d6634c38f120df6c787262edc9cc5
vaishu-2716 <vaishuvarma27@gmail.com>
2026-05-18 17:32:25 +0530

    Updated production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "baseURL: "https://bug-tracker-system-nrho.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
