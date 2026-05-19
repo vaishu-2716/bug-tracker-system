09a5e0e82c651c30829fa86b4711d525aa396816
vaishu-2716 <vaishuvarma27@gmail.com>
2026-05-19 14:09:56 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -7,32 +8,34 @@ const bugRoutes = require("./routes/bugRoutes");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+// CORS Configuration
+app.use(
+  cors({
+    origin: "https://bug-tracker-system-six.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
+// Middleware
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Bug Tracker API Running");
 });
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/bugs", bugRoutes);
 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+// Server Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
