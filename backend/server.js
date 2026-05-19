7e662e77800ba9a7fc4a4834aeae4da9a386b23b
vaishu-2716 <vaishuvarma27@gmail.com>
2026-05-19 14:16:20 +0530

    Install cors

--- backend/server.js
+++ backend/server.js
@@ -8,34 +8,27 @@ const bugRoutes = require("./routes/bugRoutes");
 
 const app = express();
 
-// CORS Configuration
-app.use(
-  cors({
-    origin: "https://bug-tracker-system-six.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// CORS
+app.use(cors({
+  origin: "https://bug-tracker-system-six.vercel.app"
+}));
 
 // Middleware
 app.use(express.json());
 
-// Test Route
+// Routes
 app.get("/", (req, res) => {
   res.send("Bug Tracker API Running");
 });
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/bugs", bugRoutes);
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+// MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-// Server Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
